8f466a253455f4eb263517653105231171c1252f
Maksym-Kalistratov <4702323@gmail.com>
2025-10-28 16:14:48 +0100

    Init

---
+++ BYT_Solo_HW3/BYT_Solo_HW3/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace BYT_Solo_HW3;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
